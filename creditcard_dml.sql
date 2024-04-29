65237dd1afac4b17e9732131218732a23616dcf3
nouhben <benkadi.nouh@icloud.com>
2024-04-29 14:38:58 +0100

    Groupe by having

--- creditcard_dml.sql
+++ creditcard_dml.sql
@@ -1,1 +1,31 @@
 USE financial_db;
+-- Write a query that will display the number of records in the table.
+SELECT  Creditcard_company
+FROM creditcard
+;
+-- Write a query that will display a list 
+-- of credit cards, 
+-- ordered by the highest credit limit
+
+SELECT DISTINCT CreditcardNum,CardHolder, Credit_Limit
+FROM creditcard
+ORDER BY Credit_Limit DESC LIMIT 1;
+SELECT CreditcardNum, Credit_Limit 
+FROM creditcard 
+WHERE Credit_Limit = (
+    SELECT MAX(Credit_Limit) 
+    FROM creditcard 
+    );
+
+SELECT `CreditcardNum`, `Credit_Limit` 
+FROM creditcard
+WHERE `Credit_Limit` IN (1200,3000,4500)
+ORDER BY `Credit_Limit`;
+
+SELECT CreditcardNum,CardHolder, City
+FROM creditcard
+WHERE `City` <> 'London';
+
+SELECT COUNT(`CreditcardNum`) as NumberOfMasters
+FROM creditcard
+WHERE `Creditcard_type` = 'Mastercard';
